8ec66aa4c9342975102346d4e77f6711e1e8f58c
Eddy Barales <erbarales@gyt.com.gt>
2021-10-29 12:57:04 -0600

    MOD: Se cambia el orden.

--- run-k8s.ps1
+++ run-k8s.ps1
@@ -1,5 +1,5 @@
 kubectl apply -f kubernetes\secret.yml
 kubectl apply -f kubernetes\configmap.yml
-kubectl apply -f kubernetes\deploy.yml
 kubectl apply -f kubernetes\service.yml
+kubectl apply -f kubernetes\deploy.yml
 kubectl port-forward service/my-java-service 8080:8080
